de8448895c50db328a54d19219e43d581fe7bd63
NOOOku <kayrsapiev@gmail.com>
2025-06-04 16:11:22 +0500

    Fixed datetime import and usage in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+import datetime
 from typing import Union
 from jose import JWTError, jwt
 import pyotp
@@ -20,12 +20,12 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
+def create_access_token(data: dict, expires_delta: Union[datetime.timedelta, None] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
